1492e41c5fab020790264871be574043c1274aeb
Sylvia Llorente <sylviall81@gmail.com>
2024-03-04 14:04:37 +0100

    primer ejercicio variables js foundation

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+console.log (6+9);
+
+console.log (4+5+6+7+8+9);
+
+console.log((4 + 6 + 9)/77);
+
+let a = 10;
+
+console.log(a * 9);
+
+let b = 7 * a;
+console.log(b)
+
+let max = 57;
+let actual = max-13;
+
+let percentage = (actual/max);
+
+console.log(percentage);
+
